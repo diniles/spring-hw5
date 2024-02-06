56204cebedc2975e58c1eb48bee02086b38f21c8
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-06 16:40:27 +0300

    Feat: interface TaskRepository

---
+++ src/main/java/ru/gb/hw5/TaskRepository.java
@@ -0,0 +1,14 @@
+package ru.gb.hw5;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByOrderByDateAsc();
+
+    List<Task> findAllByStateIsOrderByDate(String state);
+}
