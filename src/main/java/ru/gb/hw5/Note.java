19592e6d55a58e0d557f51fbbb366556ab267b9d
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-06 16:20:56 +0300

    Feat: class Note

---
+++ src/main/java/ru/gb/hw5/Note.java
@@ -0,0 +1,56 @@
+package ru.gb.hw5;
+
+import java.util.Date;
+
+public class Note {
+    private Long id;
+    private String author;
+    private String title;
+    private String content;
+    private Status status;
+    private Date creationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
---
+++ src/main/java/ru/gb/hw5/Status.java
@@ -0,0 +1,5 @@
+package ru.gb.hw5;
+
+public enum Status {
+    NOT_STARTED, IN_PROGRESS, FINISHED;
+}
